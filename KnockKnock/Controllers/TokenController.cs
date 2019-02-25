8caea6d9dc13d46fb8c9bdafc8aecc86123742c0
Rahul Bedge <rahul.bedge@gmail.com>
2019-02-26 01:21:07 +1100

    Added token controller.

---
+++ KnockKnock/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using KnockKnock.Interfaces;
+using KnockKnock.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnockKnock.Controllers
+{
+    [Route("api/Token")]
+    [ApiController]
+    [Produces("application/json")]
+    public class TokenController : ControllerBase
+    {
+        public TokenController()
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(long), StatusCodes.Status200OK)]
+        public IActionResult Get()
+        {
+            //string token = "72e0b481-72b8-4fc9-bb7f-a259b4f92ae7"; 
+            string token = "72e0b481-72b8-4fc9-bbbb-a259b4f92abb"; 
+            return Ok(token);
+        }
+    }
+}
