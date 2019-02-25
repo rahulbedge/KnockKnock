b38fde9fe2ee5a4ffdf3a0efd597ea9542352538
Rahul Bedge <rahul.bedge@gmail.com>
2019-02-25 16:10:43 +1100

    Added gzip compression, cache control and X-Content-Type-Options

--- KnockKnock/Startup.cs
+++ KnockKnock/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.IO.Compression;
 using KnockKnock.Interfaces;
 using KnockKnock.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace KnockKnock
 {
@@ -30,6 +24,14 @@ namespace KnockKnock
         {
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddResponseCompression(opt =>
+            {
+                opt.Providers.Add<GzipCompressionProvider>();
+                opt.EnableForHttps = true;
+            });
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level =
+            CompressionLevel.Fastest);
+
             services.AddScoped<INumberSeries, FibonacciService>();
             services.AddScoped<ITextService, TextService>(); 
         }
@@ -48,6 +50,14 @@ namespace KnockKnock
             }
 
             app.UseHttpsRedirection();
+            app.UseResponseCompression();
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+                context.Response.Headers.Add("Expires", "-1");
+                await next();
+            });
             app.UseMvc();
         }
     }
